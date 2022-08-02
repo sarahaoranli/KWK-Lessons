3ccddb735a0a970a1bffe019e1b648b40d34e3b4
Haoran Li <68714456+sarahaoranli@users.noreply.github.com>
2022-08-02 10:38:52 -0400

    Add files via upload

---
+++ ToDoList/ToDoList/ToDoList/AddToDoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddToDoViewController.swift
+//  ToDoList
+//
+//  Created by scholar on 8/1/22.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    var previousToDoTVC = ToDoListTableViewController()
+    @IBOutlet weak var descriptionInput: UITextField!
+    @IBOutlet weak var switchInput: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        let newToDo = ToDoClass()
+        
+        if let checkForInput = descriptionInput.text {
+            newToDo.description = checkForInput
+            newToDo.important = switchInput.isOn
+        }
+        
+        previousToDoTVC.listOfToDo.append(newToDo)
+        previousToDoTVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+    }
+}
---
+++ ToDoList/ToDoList/ToDoList/CompleteToDoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CompleteToDoViewController.swift
+//  ToDoListUITests
+//
+//  Created by scholar on 8/2/22.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+    
+    
+    @IBOutlet weak var annoyingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeTapped(_ sender: UIButton) {
+    }
+
+}
